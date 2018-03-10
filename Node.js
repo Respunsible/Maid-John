dc8a112147d511229ed2ee6234d7f85c43e94d49
Respunsible <37230922+Respunsible@users.noreply.github.com>
2018-03-10 13:19:33 -0500

    Update Node.js

--- Node.js
+++ Node.js
@@ -2,8 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('message', message => {
-    if (message.content.toLowerCase() === 'hi') {
-        message.channel.send('hello');
+    if (message.content.toLowerCase() === 'hi' || message.content.toLowerCase() === 'hi maid john') {
+        message.channel.send('Hello', message.author.username);
     }
 });
 
