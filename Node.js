0746f840a82da3c527863038e3532b84be97b8d5
Respunsible <37230922+Respunsible@users.noreply.github.com>
2018-03-10 14:34:42 -0500

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,9 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var names = ['maid john', 'best girl', 'best girl john', 'girl john', 'guys', 'everyone']
+var greetings = ['hi', 'hello', 'hey', 'yo', 'greetings']
+
 client.on('message', message => {
-    if (message.content.toLowerCase() === 'hi' || message.content.toLowerCase() === 'hi maid john') {
-        message.channel.send('Hello ' + message.author.username);
+    message.content.toLowerCase()
+    if (greetings.includes(message.content) || greetings.includes(message.content) && names.includes(message.content)) {
+        message.channel.send('Hello ' + message.author.username + '.');
     }
 });
 
