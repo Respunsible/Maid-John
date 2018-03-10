fe04d9f5dd5e57d99ead8ee7c2478adc607220cf
Respunsible <37230922+Respunsible@users.noreply.github.com>
2018-03-10 18:37:16 -0500

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,22 +1,25 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const greetings = ['hi', 'hello', 'hey', 'yo', 'greetings']
-const names = ['maid john', 'best girl', 'best girl john', 'girl john', 'guys', 'everyone']
+var message1 = nil
+
+const greetings = ['hi', 'hello', 'hey', 'yo', 'greetings'];
+const names = ['maid john', 'best girl', 'best girl john', 'girl john', 'guys', 'everyone'];
 
 const isIncludes = (message, arr) => {
-	for (let i in arr) {
-		if (message.includes(arr[i])) {
-			return true;
-		}
-	}
-	return false;
+  for (let i in arr) {
+    if (message.includes(arr[i])) {
+      return true;
+    }
+  }
+  return false;
 }
 
 client.on('message', message => {
-	if (isIncludes(message.content[0], greetings) && isIncludes(message.content[0], names)) {
-		message.channel.send('Hello ' + message.author.username + '.');
-	}
+  message1 = message.content
+  if (isIncludes(message1[0], greetings) && isIncludes(message1[0], names)) {
+    message.channel.send('Hello ' + message.author.username + '.');
+  }
 });
 
 client.login(process.env.token);
