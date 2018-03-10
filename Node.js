d3c0e09bf9dea073bbac084b35243b6f2ffd5184
Respunsible <37230922+Respunsible@users.noreply.github.com>
2018-03-10 12:55:50 -0500

    Update Node.js

--- Node.js
+++ Node.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('message', message => {
     if (message.content === 'hi') {
-    	message.createMessage('hello');
+    	message.send('hello');
   	}
 });
 
