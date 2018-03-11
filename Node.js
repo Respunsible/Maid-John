b56f053892385a03a21d6327f487ab708b507a44
Respunsible <37230922+Respunsible@users.noreply.github.com>
2018-03-10 20:14:38 -0500

    Update Node.js

--- Node.js
+++ Node.js
@@ -4,9 +4,9 @@ const client = new Discord.Client();
 const greetings = ['hi', 'hello', 'hey', 'yo', 'greetings'];
 const names = ['maid john', 'best girl', 'best girl john', 'girl john', 'guys', 'everyone', 'girls', 'maids'];
 
-const isIncludes = (message, arr) => {
+const isIncludes = (msg, arr) => {
   for (let i in arr) {
-    if (message.includes(arr[i])) {
+    if (msg.includes(arr[i])) {
       return true;
     }
   }
