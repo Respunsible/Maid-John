3f2ee445d005d6f1df9dafb65e542eac093faa2f
Respunsible <37230922+Respunsible@users.noreply.github.com>
2018-03-10 21:51:26 -0500

    Update Node.js

--- Node.js
+++ Node.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 const greetings = ['hi', 'hello', 'hey', 'yo', 'greetings'];
 const names = ['maid john', 'best girl', 'best girl john', 'girl john', 'guys', 'everyone', 'girls', 'maids'];
-const what = ['what', 'wat', 'wut']
+const what = ['what', 'wat', 'wut'];
 
 const includes = (message, arr) => {
   for (let i in arr) {
@@ -15,7 +15,7 @@ const includes = (message, arr) => {
 }
 
 client.on('message', message => {
-  if (what.some(item => message.content.includes('${item} version are you'))) {
+  if (what.some(item => message.content.includes(`${item} version are you`))) {
     message.channel.send('I\'m version 0.0.3')
   }
   if (includes([message.content][0], greetings) && includes([message.content][0], names)) {
