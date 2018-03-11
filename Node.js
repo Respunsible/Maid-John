fbfc51b7a0092886327f95813de582fa6bb6e457
Respunsible <37230922+Respunsible@users.noreply.github.com>
2018-03-10 20:02:55 -0500

    Update Node.js

--- Node.js
+++ Node.js
@@ -2,23 +2,23 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const greetings = ['hi', 'hello', 'hey', 'yo', 'greetings'];
-const names = ['maid john', 'best girl', 'best girl john', 'girl john', 'guys', 'everyone'];
+const names = ['maid john', 'best girl', 'best girl john', 'girl john', 'guys', 'everyone', 'girls', 'maids'];
 
-const isIncludes = (msg, arr) => {
+const includes = (message, arr) => {
   for (let i in arr) {
-    if (msg.includes(arr[i])) {
-      return true
+    if (message.includes(arr[i])) {
+      return true;
     }
   }
   return false;
 }
 
 client.on('message', message => {
-  message.content.toLowerCase();
-  if (isIncludes([message.content][0], names) && message.content.includes('what is version are you')) {
-    message.channel.send('I\'m version 0.0.1')
+  message = message.content.toLowerCase();
+  if (message.content.includes('what version are you') && includes([message.content][0], names) &&) {
+    message.channel.send('I\'m version 0.0.2')
   }
-  if (isIncludes([message.content][0], greetings) && isIncludes([message.content][0], names)) {
+  if (includes([message.content][0], greetings) && includes([message.content][0], names)) {
     message.channel.send('Hello ' + message.author.username + '.');
   }
 });
