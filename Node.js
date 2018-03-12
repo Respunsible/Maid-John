0f7e24ec235b319d10541e00250628c28c7618e2
Respunsible <37230922+Respunsible@users.noreply.github.com>
2018-03-12 12:51:54 -0400

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,5 +1,5 @@
 var maid_johns_names = ['maid john', 'best girl', 'best girl john', 'girl john', 'guys', 'everyone', 'girls', 'maids', 'john maid', 'john best girl'];
-var maid_johns_version = '0.0.2'
+var maid_johns_version = '0.0.1'
 
 //Dictionary
 var are = ['are', 'r'].join("|");
@@ -16,7 +16,7 @@ client.on('message', message => {
   //Converts message content to lowercase.
   message.content = message.content.toLowerCase();
   
-  if (RegExp('(' + what + ')\\s+(' + version + ')\\s+(' + are + ')\\s+(' + you + ')').test(message.content) || RegExp('(' + what + ')\\s+(' + 'is' + ')\\s+(' + your + ')\\s+(' + version + ')').test(message.content) && maid_johns_names.some(maid_johns_name => message.content.includes(maid_johns_name))) {
+  if (RegExp('(' + what + ')\\s+(' + version + ')\\s+(' + are + ')\\s+(' + you + ')').test(message.content) && maid_johns_names.some(maid_johns_name => message.content.includes(maid_johns_name)) || RegExp('(' + what + ')\\s+(' + 'is' + ')\\s+(' + your + ')\\s+(' + version + ')').test(message.content) && maid_johns_names.some(maid_johns_name => message.content.includes(maid_johns_name))) {
     message.channel.send(maid_johns_version);
   }
 });
