9f7de1bdd95173c2212cc7303d6235d345c5ce64
Respunsible <37230922+Respunsible@users.noreply.github.com>
2018-03-12 12:19:43 -0400

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,4 +1,5 @@
 var maid_johns_names = ['maid john', 'best girl', 'best girl john', 'girl john', 'guys', 'everyone', 'girls', 'maids', 'john maid', 'john best girl'];
+var msg = 'what ver r'
 
 //Dictionary
 var are = ['are', 'r'];
@@ -14,7 +15,7 @@ client.on('message', message => {
   //Converts message content to lowercase.
   message.content = message.content.toLowerCase();
   //If message content = 'What version are you?'
-  if (new RegExp("^.*(" + what + ")\\s+.*(" + version + ")\\s+(" + are + ").*$").test(message.content)) {
+  if (new RegExp("^.*(" + what + ")\\s+.*(" + version + ")\\s+(" + are + ").*$").test(msg)) {
     //&& maid_johns_names.some(maid_johns_name => message.content.includes(maid_johns_name))
     message.channel.send('I\'m version 0.0.1');
   }
