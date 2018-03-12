02071a8bccf8b302ba9d1c26cdad23c9380ef6dd
Respunsible <37230922+Respunsible@users.noreply.github.com>
2018-03-12 12:44:30 -0400

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,4 +1,5 @@
 var maid_johns_names = ['maid john', 'best girl', 'best girl john', 'girl john', 'guys', 'everyone', 'girls', 'maids', 'john maid', 'john best girl'];
+var maid_johns_version = '0.0.2'
 
 //Dictionary
 var are = ['are', 'r'].join("|");
@@ -6,6 +7,7 @@ var hi = ['hi', 'hello', 'hey', 'hai'].join("|");
 var version = ['version', 'ver', 'ver.'].join("|");
 var what = ['what', 'wat', 'wut'].join("|");
 var you = ['you', 'u'].join("|");
+var your = ['your', 'ur'].join("|");
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -13,10 +15,11 @@ const client = new Discord.Client();
 client.on('message', message => {
   //Converts message content to lowercase.
   message.content = message.content.toLowerCase();
-  //If message content includes 'what version are you' and Maid John's name
-  if (RegExp('(' + what + ')\\s+(' + version + ')\\s+(' + are + ')').test(message.content) && maid_johns_names.some(maid_johns_name => message.content.includes(maid_johns_name))) {
-    message.channel.send('I\'m Maid John 0.0.1');
-  }
+  
+  if (RegExp('(' + what + ')\\s+(' + version + ')\\s+(' + are + ')\\s+(' + you + ')').test(message.content) || RegExp('(' + what + ')\\s+(' + 'is' + ')\\s+(' + your + ')\\s+(' + version + ')').test(message.content) && maid_johns_names.some(maid_johns_name => message.content.includes(maid_johns_name))) {
+    message.channel.send(maid_johns_version);
+  } else if () 
+  
 });
 
 client.login(process.env.token);
