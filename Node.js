88429625a6120d1e78ab5db99179cff938029f79
Respunsible <37230922+Respunsible@users.noreply.github.com>
2018-03-12 13:12:16 -0400

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,13 +1,13 @@
-var maid_johns_names = ['maid john', 'best girl', 'best girl john', 'girl john', 'guys', 'everyone', 'girls', 'maids', 'john maid', 'john best girl'].join("|");
-var maid_johns_version = '0.0.1'
+var maid_johns_names = ['maid john', 'best girl', 'best girl john', 'girl john', 'guys', 'everyone', 'girls', 'maids', 'john maid', 'john best girl'];
+var maid_johns_version = '0.0.1';
 
 //Dictionary
-var are = ['are', 'r'].join("|");
-var hi = ['hi', 'hello', 'hey', 'hai'].join("|");
-var version = ['version', 'ver', 'ver.'].join("|");
-var what = ['what', 'wat', 'wut'].join("|");
-var you = ['you', 'u'].join("|");
-var your = ['your', 'ur'].join("|");
+var are = ['are', 'r'];
+var hi = ['hi', 'hello', 'hey', 'hai'];
+var version = ['version', 'ver', 'ver.'];
+var what = ['what', 'wat', 'wut'];
+var you = ['you', 'u'];
+var your = ['your', 'ur'];
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -16,7 +16,7 @@ client.on('message', message => {
   //Converts message content to lowercase.
   message.content = message.content.toLowerCase();
   
-  if (RegExp('(' + what + ')\\s+(' + version + ')\\s+(' + are + ')\\s+(' + you + ')').test(message.content) && maid_johns_names.some(maid_johns_name => message.content.includes(maid_johns_name)) || RegExp('(' + what + ')\\s+(' + 'is' + ')\\s+(' + your + ')\\s+(' + version + ')').test(message.content) && maid_johns_names.some(maid_johns_name => message.content.includes(maid_johns_name))) {
+  if (RegExp('(' + what.join("|") + ')\\s+(' + version.join("|") + ')\\s+(' + are.join("|") + ')\\s+(' + you.join("|") + ')').test(message.content) && maid_johns_names.some(maid_johns_name => message.content.includes(maid_johns_name)) || RegExp('(' + what.join("|") + ')\\s+(' + 'is' + ')\\s+(' + your.join("|") + ')\\s+(' + version.join("|") + ')').test(message.content) && maid_johns_names.some(maid_johns_name => message.content.includes(maid_johns_name))) {
     message.channel.send(maid_johns_version);
   }
 });
