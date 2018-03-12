312a9bad32cd0156657cb6119b54dbe9a8318fee
Respunsible <37230922+Respunsible@users.noreply.github.com>
2018-03-11 21:11:41 -0400

    Update Node.js

--- Node.js
+++ Node.js
@@ -3,24 +3,17 @@ const client = new Discord.Client();
 
 var greetings = ['hi', 'hello', 'hey', 'yo', 'greetings'];
 var names = ['maid john', 'best girl', 'best girl john', 'girl john', 'guys', 'everyone', 'girls', 'maids'];
-var whats = ['what', 'wat', 'wut'];
 var versions = ['version', 'ver', 'ver.'];
-
-const includes = (message, arr) => {
-  for (let i in arr) {
-    if (message.includes(arr[i])) {
-      return true;
-    }
-  }
-  return false;
-}
+var whats = ['what', 'wat', 'wut'];
 
 client.on('message', message => {
-  //what
-  const includeswhat = whats.some(what => message.content.includes(what))
   
-  if ('wat' + 'ver') {
-    message.channel.send('I\'m version 0.0.3');
+  const message_includes_name = names.some(name => message.content.includes(name))
+  const message_includes_version = versions.some(version => message.content.includes(version))
+  const message_includes_what = whats.some(what => message.content.includes(what))
+  
+  if (message.content.includes('what' + 'version')) {
+    message.channel.send('I\'m version 0.0.4');
   }
   if (includes([message.content][0], greetings) && includes([message.content][0], names)) {
     message.channel.send('Hello ' + message.author.username + '.');
