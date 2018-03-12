1b95c88102acba992b683b22d4678613ebe43503
Respunsible <37230922+Respunsible@users.noreply.github.com>
2018-03-12 16:46:53 -0400

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,10 +1,12 @@
-var maid_johns_his2 = ['Hi.', 'Hello.']
+var maid_johns_byes = ['Bye.', 'Take care.']
+var maid_johns_his = ['Hi.', 'Hello.']
 var maid_johns_names = ['maid john', 'best girl', 'best girl john', 'girl john', 'guys', 'everyone', 'girls', 'maids', 'john maid', 'john best girl'];
 var maid_johns_version = '0.0.2';
 
 var john = 'd to the e to the d'
 
 var ares = ['are', 'r'];
+var byes = ['bye', 'goodbye', 'see ya', 'cya', 'see you']
 var his = ['hi', 'hello', 'hey', 'hai', 'yo'];
 var loves = ['love', 'luv']
 var versions = ['version', 'ver', 'ver.'];
@@ -19,20 +21,28 @@ client.on('message', message => {
   
   var maid_johns_his2 = ['Hi', 'Hello', 'Hi' + ' ' + message.author.username + '.', 'Hello' + ' ' + message.author.username + '.']
   
-  //Converts message content to lowercase.
+  //Lowercase
   message.content = message.content.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, ' ').toLowerCase();
   
-  //Maid John's version
+  //Version
   if (RegExp('(' + whats.join("|") + ')\\s+(' + versions.join("|") + ')\\s+(' + ares.join("|") + ')\\s+(' + yous.join("|") + ')').test(message.content) && maid_johns_names.some(maid_johns_name => message.content.includes(maid_johns_name)) || RegExp('(' + whats.join("|") + ')\\s+(' + 'is' + ')\\s+(' + yours.join("|") + ')\\s+(' + versions.join("|") + ')').test(message.content) && maid_johns_names.some(maid_johns_name => message.content.includes(maid_johns_name))) {
     message.channel.send(maid_johns_version);
   } else if (message.isMentioned(client.user) && RegExp('(' + whats.join("|") + ')\\s+(' + versions.join("|") + ')\\s+(' + ares.join("|") + ')\\s+(' + yous.join("|") + ')').test(message.content) || message.isMentioned(client.user) && RegExp('(' + whats.join("|") + ')\\s+(' + 'is' + ')\\s+(' + yours.join("|") + ')\\s+(' + versions.join("|") + ')').test(message.content)) {
     message.reply(maid_johns_version);
   }
   
+  //Hi
   if (RegExp('(' + his.join("|") + ')\\s+(' + maid_johns_names.join("|") + ')').test(message.content)) {
     message.channel.send(maid_johns_his2[Math.floor(Math.random() * maid_johns_his2.length)]);
   } else if (message.isMentioned(client.user) && RegExp('(' + his.join("|") + ')').test(message.content)) {
      message.reply(maid_johns_his[Math.floor(Math.random() * maid_johns_his.length)]);
+  }
+  
+  //Bye
+  if (RegExp('(' + byes.join("|") + ')\\s+(' + maid_johns_names.join("|") + ')').test(message.content)) {
+    message.channel.send(maid_johns_byes2[Math.floor(Math.random() * maid_johns_byes2.length)]);
+  } else if (message.isMentioned(client.user) && RegExp('(' + byes.join("|") + ')').test(message.content)) {
+     message.reply(maid_johns_byes[Math.floor(Math.random() * maid_johns_byes.length)]);
   }
   
   if (message.author.username === john && RegExp('(' + 'i' + ')\\s+(' + loves.join("|") + ')\\s+(' + you.join("|") + ')').test(message.content) && maid_johns_names.some(maid_johns_name => message.content.includes(maid_johns_name))) {
