ed085ef156e13a625c0f4f790bfe6be590054449
Respunsible <37230922+Respunsible@users.noreply.github.com>
2018-03-12 15:04:26 -0400

    Update Node.js

--- Node.js
+++ Node.js
@@ -25,7 +25,7 @@ client.on('message', message => {
   
   if (RegExp('(' + hi.join("|") + ')\\s+(' + maid_johns_names.join("|") + ')').test(message.content)) {
     message.channel.send(maid_johns_hi[Math.floor(Math.random() * maid_johns_hi.length)]);
-  } else if (message.isMentioned(client.user) && hi.includes(message.content.split(' ')[1])) {
+  } else if (message.isMentioned(client.user) && hi.includes(message.content.split(' ')[0])) {
      message.reply(maid_johns_hi[Math.floor(Math.random() * maid_johns_hi.length)]);
   }
 });
