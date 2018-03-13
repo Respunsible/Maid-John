65bad3f6825fcdbe0bdfe14744796a118523f0f2
Respunsible <37230922+Respunsible@users.noreply.github.com>
2018-03-13 14:51:59 -0400

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,7 +1,7 @@
 var maid_johns_byes = ['Bye.', 'Take care.'];
 var maid_johns_his = ['Hi.', 'Hello.'];
 var maid_johns_names = ['maid john', 'best girl', 'best girl john', 'girl john', 'guys', 'everyone', 'girls', 'maids', 'john maid', 'john best girl'];
-var maid_johns_version = '0.0.3';
+var maid_johns_version = '0.0.4';
 
 var ares = ['are', 'r'];
 var byes = ['bye', 'goodbye', 'see ya', 'cya', 'see you'];
@@ -53,11 +53,11 @@ client.on('message', message => {
   }
   
   //Rock-paper-scissors
-  if (message.author.id !== '423186959712780298') {
-    if (message.content === 'rock' || message.content === 'paper' || message.content === 'scissors' || message.content === ':fist:' || message.content === ':hand_splayed:' || message.content === ':v:' || message.content === ':scissors:') {
-      message.channel.send(rock_paper_scissors[Math.floor(Math.random() * rock_paper_scissors.length)]);
-    }
-  }
+  //if (message.author.id !== '423186959712780298') {
+    //if (message.content === 'rock' || message.content === 'paper' || message.content === 'scissors' || message.content === ':fist:' || message.content === ':hand_splayed:' || message.content === ':v:' || message.content === ':scissors:') {
+      //message.channel.send(rock_paper_scissors[Math.floor(Math.random() * rock_paper_scissors.length)]);
+    //}
+  //}
   
   if (message.author.id === '215956987815526400' && RegExp('(' + 'i' + ')\\s+(' + loves.join("|") + ')\\s+(' + yous.join("|") + ')').test(message.content) && maid_johns_names.some(maid_johns_name => message.content.includes(maid_johns_name))) {
     message.channel.send('I love you too, John.');
