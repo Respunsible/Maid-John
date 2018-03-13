2c53c2751904dce3126bad5ab12433147d64872c
Respunsible <37230922+Respunsible@users.noreply.github.com>
2018-03-12 20:18:15 -0400

    Update Node.js

--- Node.js
+++ Node.js
@@ -23,36 +23,38 @@ client.on('message', message => {
   var maid_johns_byes2 = ['Bye.', 'Take care.', 'Bye' + ' ' + message.author.username + '.', 'Take care' + ' ' + message.author.username + '.']
   var maid_johns_his2 = ['Hi', 'Hello', 'Hi' + ' ' + message.author.username + '.', 'Hello' + ' ' + message.author.username + '.']
   
-  //Lowercase
-  message.content = message.content.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, ' ').toLowerCase();
+  //Converts to lowercase
+  message.content = message.content.toLowerCase();
+  //Replaces special characters
+  message.content = message.content.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, ' ')
   
-  //Version
+  //maid_johns_version
   if (RegExp('(' + whats.join("|") + ')\\s+(' + versions.join("|") + ')\\s+(' + ares.join("|") + ')\\s+(' + yous.join("|") + ')').test(message.content) && maid_johns_names.some(maid_johns_name => message.content.includes(maid_johns_name)) || RegExp('(' + whats.join("|") + ')\\s+(' + 'is' + ')\\s+(' + yours.join("|") + ')\\s+(' + versions.join("|") + ')').test(message.content) && maid_johns_names.some(maid_johns_name => message.content.includes(maid_johns_name))) {
     message.channel.send(maid_johns_version);
   } else if (message.isMentioned(client.user) && RegExp('(' + whats.join("|") + ')\\s+(' + versions.join("|") + ')\\s+(' + ares.join("|") + ')\\s+(' + yous.join("|") + ')').test(message.content) || message.isMentioned(client.user) && RegExp('(' + whats.join("|") + ')\\s+(' + 'is' + ')\\s+(' + yours.join("|") + ')\\s+(' + versions.join("|") + ')').test(message.content)) {
     message.reply(maid_johns_version);
   }
   
-  //Hi
+  //'Hi'
   if (RegExp('(' + his.join("|") + ')\\s+(' + maid_johns_names.join("|") + ')').test(message.content)) {
     message.channel.send(maid_johns_his2[Math.floor(Math.random() * maid_johns_his2.length)]);
   } else if (message.isMentioned(client.user) && RegExp('(' + his.join("|") + ')').test(message.content)) {
      message.reply(maid_johns_his[Math.floor(Math.random() * maid_johns_his.length)]);
   }
   
-  //Bye
+  //'Bye'
   if (RegExp('(' + byes.join("|") + ')\\s+(' + maid_johns_names.join("|") + ')').test(message.content)) {
     message.channel.send(maid_johns_byes2[Math.floor(Math.random() * maid_johns_byes2.length)]);
   } else if (message.isMentioned(client.user) && RegExp('(' + byes.join("|") + ')').test(message.content)) {
      message.reply(maid_johns_byes[Math.floor(Math.random() * maid_johns_byes.length)]);
   }
   
-  //KYS
+  //'I will not'
   if (RegExp('(' + kys.join("|") + ')\\s+(' + maid_johns_names.join("|") + ')').test(message.content) || RegExp('(' + maid_johns_names.join("|") + ')\\s+(' + kys.join("|") + ')').test(message.content)) {
     message.channel.send('I will not.');
   }
   
-  if (message.author.id === '215956987815526400' && RegExp('(' + 'i' + ')\\s+(' + loves.join("|") + ')\\s+(' + you.join("|") + ')').test(message.content) && maid_johns_names.some(maid_johns_name => message.content.includes(maid_johns_name))) {
+  if (message.author.id === '418108591468314644' && RegExp('(' + 'i' + ')\\s+(' + loves.join("|") + ')\\s+(' + you.join("|") + ')').test(message.content) && maid_johns_names.some(maid_johns_name => message.content.includes(maid_johns_name))) {
       message.channel.send('I still love you too, but you\'re too much for me.');
   }
 });
