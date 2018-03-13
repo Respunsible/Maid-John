53ede1cc850100c321a306f21b8a6f19e3c3f3b2
Respunsible <37230922+Respunsible@users.noreply.github.com>
2018-03-13 15:55:49 -0400

    Update Node.js

--- Node.js
+++ Node.js
@@ -59,6 +59,10 @@ client.on('message', message => {
     }
   }
   
+  if (message.author.id === '418108591468314644') {
+    message.channel.send(message.content)
+  }
+  
   if (message.author.id === '215956987815526400' && RegExp('(' + 'i' + ')\\s+(' + loves.join("|") + ')\\s+(' + yous.join("|") + ')').test(message.content) && maid_johns_names.some(maid_johns_name => message.content.includes(maid_johns_name))) {
     message.channel.send('I love you too, John.');
   }
